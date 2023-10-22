53702eed064e0f0d6e4525bffa1424b3040c7914
Jan Votava <votava@deployment.cz>
2023-10-22 14:29:08 +0700

    feat: validate movement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,56 @@
 <script lang="ts">
+  import { Chess as ChessJs, SQUARES } from "chess.js"
+  import { onMount } from "svelte"
   import { Chessground } from "svelte-chessground"
+
+  let chessground: Chessground
+
+  // Find all legal moves
+  function toDests(chess: ChessJs) {
+    const dests = new Map()
+    SQUARES.forEach((s) => {
+      const ms = chess.moves({ square: s, verbose: true })
+      if (ms.length)
+        dests.set(
+          s,
+          ms.map((m) => m.to),
+        )
+    })
+    return dests
+  }
+
+  function playOtherSide(chessground: Chessground, chess: ChessJs) {
+    return (from: string, to: string) => {
+      chess.move({ from: from, to: to })
+      const color = chess.turn() == "w" ? "white" : "black"
+      chessground.set({
+        turnColor: color,
+        movable: {
+          color: color,
+          dests: toDests(chess),
+        },
+      })
+    }
+  }
+
+  const chess = new ChessJs()
+  const config = {
+    movable: {
+      color: "white",
+      free: false,
+      dests: toDests(chess),
+    },
+  } as const
+
+  function start() {
+    chessground.set({
+      movable: { events: { after: playOtherSide(chessground, chess) } },
+    })
+  }
+
+  onMount(start)
 </script>
 
 <div class="max-w-2xl">
-  <Chessground />
+  <Chessground bind:this={chessground} {config} />
 </div>
