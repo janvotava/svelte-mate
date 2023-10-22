a7aa5b44f25b672e280cf6ee0fde2becd5a2cf43
Jan Votava <votava@deployment.cz>
2023-10-22 20:04:45 +0700

    fix: stockfish not loading in CORS env because cors
    Apparently Vite cors is still needed for static files.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vitest/config"
+import type { Plugin } from "vite"
+
+const viteServerConfig: Plugin = {
+  name: "Add CORS headers",
+  configureServer: (server) => {
+    server.middlewares.use((_req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      res.setHeader("Access-Control-Allow-Methods", "GET")
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin")
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp")
+      next()
+    })
+  },
+}
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), viteServerConfig],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
   },
